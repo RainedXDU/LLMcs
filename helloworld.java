f3194703a0d0055a19459436920870d15ddc2db5
StayBun <kelaiyinblue@163.com>
2025-08-20 22:00:02 +0800

    cherry

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,11 @@
 public class helloworld {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+        System.out.println("Hello chestnut");
+        System.out.println("Hello but");
         System.out.println("Hello IDEgit");
         System.out.println("Hello staybun");
-        System.out.println("Hello chestnut");
         System.out.println("Hello chestnut");
         System.out.println("Hello amend");
     }
