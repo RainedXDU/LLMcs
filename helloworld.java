a5512a2d7c74e1f7c90b32c9252422208f5a8697
StayBun <kelaiyinblue@163.com>
2025-08-20 21:59:03 +0800

    添加文件

--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,7 @@ public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello IDEgit");
+        System.out.println("Hello staybun");
+        System.out.println("Hello chestnut");
     }
 }
